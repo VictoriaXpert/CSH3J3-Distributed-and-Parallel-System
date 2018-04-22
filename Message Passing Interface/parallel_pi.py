616430483bbf6bc6777617e9b995fe18e68a00cd
Aditya Alif Nugraha <adityaalifnugraha@gmail.com>
2018-04-22 19:35:02 +0700

    add parallel pi

--- Message Passing Interface/parallel_pi.py
+++ Message Passing Interface/parallel_pi.py
@@ -9,23 +9,23 @@ def Pi(num_steps):
 
     start = time.time()
     step = 1.0/num_steps
-    sum = 0
-    hasil = 0
+    total = 0
+    result = 0
 
     if rank == 0:
         for i in range(1, int((rank+1)*(num_steps/size))):
             x = (i+0.5)*step
-            sum += 4.0/(1.0+x*x)
-            pi = step * sum
-        hasil = pi
+            total += 4.0/(1.0+x*x)
+            pi = step * total
+        result = pi
         for i in range(1, size):
-            hasil += comm.recv(source=i)
-        print("Pi: ", hasil)
+            result += comm.recv(source=i)
+        print("Pi: ", result)
     else:
         for i in range(int(rank*num_steps/size), int((rank+1)*(num_steps/size))):
             x = (i+0.5)*step
-            sum += 4.0/(1.0+x*x)
-            pi = step*sum
+            total += 4.0/(1.0+x*x)
+            pi = step*total
         comm.send(pi, dest=0)
     end = time.time()
     print("Pi with %d steps is %f in %f secs" % (num_steps, pi, end-start))
